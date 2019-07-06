6b3b5608cda7c9904c10fb64fc9a920267bb4369
Artem Shevlyakov <artartyom@gmail.com>
2019-07-06 18:22:03 +0300

    Now works without installation

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-install_path="$(which pomodoro)"
+install_path="$0"
 real_path=$(realpath "$install_path")
-real_workdir="$(dirname $real_path)"
+real_workdir=$(dirname $real_path)
 
 timer_work=$1
 timer_rest=$2
