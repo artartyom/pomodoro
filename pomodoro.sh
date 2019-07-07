8c0a54504030b83cf95aca35196751a52901db70
Artem Shevlyakov <artartyom@gmail.com>
2019-07-07 20:39:45 +0300

    Added ability to cancel all timers and check for integer input

--- pomodoro.sh
+++ pomodoro.sh
@@ -8,7 +8,26 @@ timer_work=$1
 timer_rest=$2
 
 if [ "$timer_work" == "" ]; then
-	echo "Please specify timer duration!"
+	echo "Please use integers to specify timer duration or print pomodoro stop to stop all timers."
+	exit
+elif [ "$timer_work" == "stop" ]; then
+	echo "Stopping all pomodoros."
+	for scrnum in $(screen -list | grep Pomodoro | awk -F '.' '{print $1}'); do
+		screen -XS $scrnum quit
+	done
+	notify-send "All pomodoros stopped." "Restart pomodoros as needed." -i "$real_workdir/tomato.png"
+	exit
+fi
+
+expr $timer_work + 1 &> /dev/null
+if [ $? != 0 ]; then
+	echo "Please use integers to specify timer duration or print pomodoro stop to stop all timers."
+	exit
+fi
+
+expr $timer_rest + 1 &> /dev/null
+if [ $? != 0 ]; then
+	echo "Please use integers to specify timer duration or print pomodoro stop to stop all timers."
 	exit
 fi
 
